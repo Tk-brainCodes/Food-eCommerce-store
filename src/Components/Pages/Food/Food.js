3bae77ff07bf59ca12ac9a1c98ed9f230fefee8f
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-01-22 20:36:45 +0100

    modified

--- src/Components/Pages/Food/Food.js
+++ src/Components/Pages/Food/Food.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './Food.css';
 
 const Food = () => {
     const [food, setFood] = useState([]);
     const [loading, setLoading] = useState(false);
+
 
     const apiURL = "https://api.spoonacular.com/recipes/complexSearch?apiKey=612c448ae8c4498db639fc794229a5d0"
 
@@ -23,27 +25,32 @@ const Food = () => {
 
 
     return (
-        <div>
-            <h1>Foods</h1>
+        <div className="container__">
+            <h1>Foods Recipes</h1><br />
+            {loading ? <h1>loading...</h1> : (
 
-            <div className="food__">
-                {food.map((receipt, idx) => (
-                    <div>
-                        {receipt.title}
-                        <img src={receipt.image} alt={receipt.title} />
-                    </div>
-                ))}
-            </div>
-
+                <div className="food__">
+                    {food.map((receipt, idx) => (
+                        <div className="food__container">
+                            <div className="Header">
+                                {receipt.title}
+                            </div>
+                            <img className="img" src={receipt.image} alt={receipt.title} />
+                            <div className="price__">
+                                $ {Math.floor((Math.random() * 20) + 1)}
+                            </div>
+                            <div className="buttons__">
+                                <button className="addBtn">Add to Cart</button>
+                                <div className="Btn__love">
+                                    &#f004
+                        </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     )
 }
 
 export default Food;
-
-// <input
-// className='form-control'
-// value={query}
-// onChange={(event) => setQuery(event.target.value)}
-// placeholder='Type to search...'
-// />

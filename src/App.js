9c355810f9678eebe49e50adb8335742e2f78353
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-01-22 17:04:17 +0100

    Add parent Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 
 import './App.css';
-import { BrowserRouter as Switch, Route, Router } from 'react-router-dom';
-import Navabar from './Components/Nav/Navbar';
-import Navbar from './Components/Nav/Navbar';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import Cart from './Components/Pages/Cart/Cart';
+import Products from './Components/Pages/Products/Products';
+import Saved from './Components/Pages/Saved/Saved';
+import Navbar from './Components/Nav/Navbar'
+import Food from './Components/Pages/Food/Food';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
-        <Switch>
-          
-        </Switch>
+        <div>
+          <Route exact path="/" component={Food} />
+          <Route exact path="/saved" component={Saved} />
+          <Route exact path="/cart" component={Cart} />
+          <Route exact path="/products" component={Products} />
+        </div>
       </Router>
     </div>
   );

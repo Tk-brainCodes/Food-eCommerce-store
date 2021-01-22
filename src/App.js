021a2b02452ed4829ede2549be53b76076b106d8
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-01-22 13:57:25 +0100

    Added pages

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-
+          
         </Switch>
       </Router>
     </div>
---
+++ src/Components/Pages/Saved/Saved.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Saved = () => {
+    return (
+        <div>
+            <h1>Saved</h1>
+        </div>
+    )
+}
+
+export default Saved;

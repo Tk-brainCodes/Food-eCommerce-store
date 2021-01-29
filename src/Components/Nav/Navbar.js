41b02d8502b7762a8dd5a6005c735051cd227b54
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-01-29 11:33:46 +0100

    navigation / styles

--- src/Components/Nav/Navbar.js
+++ src/Components/Nav/Navbar.js
@@ -2,28 +2,28 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
-const Navbar = ({ cart }) => {
+const Navbar = ({ cart, getCartQuantity }) => {
     return (
         <nav>
             <div className="nav__bar">
                 <ul className="nav__items">
                     <li>
                         <NavLink exact className="list__link" activeClassName="active" to="/">
-                            <i style={{ fontSize: "20px" }} class="fas fa-utensils"></i> <br />
-                        Food
+                            <i style={{ fontSize: "20px",marginTop: "10px" }} class="fas fa-utensils"></i> <br />
+                        food
                         </NavLink>
                     </li>
                     <li>
                         <NavLink exact className="list__link" activeClassName="active" to="/saved">
-                            <i style={{ fontSize: "20px" }} className="far fa-heart"></i> <br />
+                            <i style={{ fontSize: "20px" }} class="fas fa-bookmark"></i> <br />
                         Saved
                         </NavLink>
                     </li>
                     <li>
                         <NavLink exact className="list__link" activeClassName="active" to="/cart">
                             <div className="notify__">
-                                <i syle={{ fontSize: "20px",cursor: "pointer" }} class="fas fa-shopping-cart"></i>
-                                <span>{cart.length}</span>
+                                <i syle={{ fontSize: "20px", cursor: "pointer" }} class="fas fa-shopping-cart"></i>
+                                <span>{getCartQuantity()}</span>
                             </div>
                             Cart
                         </NavLink>
@@ -31,7 +31,7 @@ const Navbar = ({ cart }) => {
                     <li>
                         <NavLink exact className="list__link" activeClassName="active" to="/products">
                             <i style={{ fontSize: "20px" }} class="fas fa-shopping-basket"></i><br />
-                        Products
+                        Menu
                         </NavLink>
                     </li>
                 </ul>

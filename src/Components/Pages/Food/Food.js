8e567732a97718c528f4343e7e14baecee22899a
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-01-29 11:37:16 +0100

    Receipt Schema

--- src/Components/Pages/Food/Food.js
+++ src/Components/Pages/Food/Food.js
@@ -1,25 +1,7 @@
-import React, { useEffect, Fragment } from 'react';
-import Receipt from './Receipt.json';
+import React, { Fragment } from 'react';
 import './Food.css';
 
-const Food = ({ loading, setLoading, food, setFood, cart, setCart }) => {
-
-    useEffect(() => {
-        const fetchUser = async () => {
-            setLoading(true); //making request
-            let userArray = [];
-            userArray.push(Receipt.results);
-            setFood(userArray[0]);
-            setLoading(false) //request received
-        }
-        fetchUser();
-    });//run once
-
-    //Add to cart
-    const addToCart = (food) => {
-        setCart([...cart, {...food}]);
-    }
-
+const Food = ({ addToCart, loading, food }) => {
     return (
         <div className="container__">
             {loading ?
@@ -30,9 +12,9 @@ const Food = ({ loading, setLoading, food, setFood, cart, setCart }) => {
                 </div>
                 : (
                     <Fragment>
-                        <h1 className="food__heading">Foods Recipes  <i className="fas fa-utensils"></i></h1><br />
+                        <h1 className="food__heading"><b>Hungry?</b> <br /> <text>order and eat  : )</text>  <i className="fas fa-utensils"></i></h1><br />
                         <div className="food__">
-                            {food.map((receipt, idx) => (
+                            {food.filter((receipt) => receipt.image).map((receipt, idx) => (
                                 <div className="food__container" key={idx}>
                                     <div className="Header">
                                         {receipt.title}
@@ -40,16 +22,25 @@ const Food = ({ loading, setLoading, food, setFood, cart, setCart }) => {
                                     <img className="img" src={receipt.image} alt={receipt.title} />
                                     <div className="price__">
                                         $ {receipt.price}
-                                    </div>
+                                    </div><br /><br />
+                                    <h5><i class="fas fa-map-marker-alt"></i> {receipt.restaurantChain}</h5><br />
                                     <div className="buttons__">
-                                        <button className="addBtn" onClick={() => addToCart(receipt)}>Add to Cart <i class="fas fa-shopping-cart"></i></button>
+                                        <div className="addBtn" onClick={() => addToCart(receipt)}>Add to Cart <i class="fas fa-shopping-cart"></i></div>
                                         <div className="Btn__love">
-                                            <i><i className="far fa-heart"></i></i>
+                                            <i><i class="far fa-bookmark"></i></i>
                                         </div>
                                     </div>
                                 </div>
                             ))}
                         </div>
+                        <div className="discover__more">
+                            <button className="discover__Btn">
+                               Discover More :)
+                            </button>
+                        </div>
+
+
+
                     </Fragment>
 
                 )}

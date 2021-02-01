f2abcea51ec981577db76664d0ddfbf9059d1183
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-02-01 09:06:24 +0100

    hero section

---
+++ src/Components/HeroSection/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeroSection = () => {
+    return (
+        <div className="hero__section">
+            <h1>Hero section</h1>
+        </div>
+    )
+}
+
+export default HeroSection;

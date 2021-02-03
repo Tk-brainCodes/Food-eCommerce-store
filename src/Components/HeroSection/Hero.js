bec7dc1281172ec7f87db02fbd37c0f9d3e9cfd1
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-02-03 10:01:01 +0100

    Logout.css file

--- src/Components/HeroSection/Hero.js
+++ src/Components/HeroSection/Hero.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './Hero.css';
+import ScrollAnimation from 'react-animate-on-scroll';
+import "animate.css/animate.min.css";
 
 const HeroSection = () => {
     return (
         <div className="hero__section">
             <div className="hero__text">
                 <div className="hero__textarea">
-                    <p className="dicover__dishes">Discover your favourite <i id="dish">Food</i></p>
-                    <p className="text">Discover more than <b id="bold">100+</b> food dish,receipts and snacks both <b id="bold_text">local</b> and <b id="bold_text">continental</b> . <br />
-                  Order and check out payment with no stress.Your food is on its way!
-                 </p>
+                    <ScrollAnimation className="animate__fadeInLeft" animateIn="fadeInLeft">
+                        <p className="dicover__dishes">Discover your favourite <i id="dish">Food Dish</i></p>
+                        <p className="text">Discover more than <b id="bold">100+</b> food dish,receipts and snacks both <b id="bold_text">local</b> and <b id="bold_text">continental</b> . <br />
+                            Order and check out payment with no stress.Your food is on its way!
+                            </p>
+                    </ScrollAnimation>
                 </div>
             </div>
             <div className="hero__image">

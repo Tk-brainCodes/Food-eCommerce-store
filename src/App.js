b770933a1876f01890ca958fabf100f3582f6a7e
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-02-03 10:04:47 +0100

    App js component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import './Components/Pages/Food/Food.css';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import Cart from './Components/Pages/Cart/Cart';
 import Products from './Components/Pages/Products/Products';
-import Saved from './Components/Pages/Saved/Saved';
 import Navbar from './Components/Nav/Navbar'
 import Food from './Components/Pages/Food/Food';
 import Receipt from './Components/Pages/Food/Receipt.json';
@@ -17,37 +16,8 @@ function App() {
   const [cart, setCart] = useState([]);
   const [food, setFood] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [save, setSave] = useState([]);
 
 
-  useEffect(() => {
-    const addedToCart = JSON.parse(localStorage.getItem('added-to-cart'));
-    if (addedToCart) {
-      setSave(addedToCart);
-    }
-  }, [])
-  const saveToLocalStorage = (items) => {
-    localStorage.setItem('added-to-cart', JSON.stringify(items));
-  }
-
-
-  //save food
-  const saveTheFood = (product) => {
-    let newSaved = [...save];
-    let itemInSave = newSaved.find((item) => product.title === item.title);
-    if (itemInSave) {
-      itemInSave.quantity++;
-    } else {
-      itemInSave = {
-        ...product,
-        quantity: 1
-      }
-      newSaved.push(itemInSave);
-    }
-    setSave(newSaved);
-    saveToLocalStorage(newSaved);
-    console.log(save, 'Saved food');
-  }
 
 
   //fetch data
@@ -87,21 +57,12 @@ function App() {
             <div>
               <Route exact path="/">
                 <Food
-                  save={save}
-                  setSave={setSave}
-                  saveTheFood={saveTheFood}
                   loading={loading}
                   setLoading={setLoading}
                   food={food}
                   setFood={setFood}
                   cart={cart}
                   setCart={setCart}
-                />
-              </Route>
-              <Route exact path="/saved">
-                <Saved
-                  save={save}
-                  setSave={setSave}
                 />
               </Route>
               <Route exact path="/cart">

96fa1247065ebabaf11b0059720cf593fba8e78b
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-02-03 10:06:22 +0100

    back button component

---
+++ src/Components/Pages/BackButton/BackBtn.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Btn.css';
+
+const BackBtn = () => {
+    return (
+        <button className="Back__Btn">
+            <i className="fas fa-angle-left"></i>
+        </button>
+    )
+}
+
+export default BackBtn;

90324b9440bd94485bfce3035e7a0953ec965e72
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-02-08 18:41:23 +0100

    add new styles

--- src/Components/HeroSection/Hero.js
+++ src/Components/HeroSection/Hero.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import './Hero.css';
-import ScrollAnimation from 'react-animate-on-scroll';
-import "animate.css/animate.min.css";
 
 const HeroSection = () => {
     return (
         <div className="hero__section">
             <div className="hero__text">
                 <div className="hero__textarea">
-                    <ScrollAnimation className="animate__fadeInLeft" animateIn="fadeInLeft">
-                        <p className="dicover__dishes">Discover your favourite <i id="dish">Food Dish</i></p>
-                        <p className="text">Discover more than <b id="bold">100+</b> food dish,receipts and snacks both <b id="bold_text">local</b> and <b id="bold_text">continental</b> . <br />
+                    <p className="dicover__dishes">Discover your favourite <i id="dish">Food Dish</i></p>
+                    <p className="text">Discover more than <b id="bold">50+</b> food dish,receipts and snacks both <b id="bold_text">local</b> and <b id="bold_text">continental</b> . <br />
                             Order and check out payment with no stress.Your food is on its way!
                             </p>
-                    </ScrollAnimation>
                 </div>
             </div>
-            <div className="hero__image">
-
+            <div className="image__conitainer">
             </div>
         </div>
     )

8aa84291c6a09148ee323fe2295d9b7a76a45804
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2021-02-26 14:43:15 +0100

    Herosection js file

--- src/Components/HeroSection/Hero.js
+++ src/Components/HeroSection/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Hero.css';
+import Table from './Table.svg'
 
 const HeroSection = () => {
     return (
@@ -10,6 +11,7 @@ const HeroSection = () => {
                     <p className="text">Discover more than <b id="bold">50+</b> food dish,receipts and snacks both <b id="bold_text">local</b> and <b id="bold_text">continental</b> . <br />
                             Order and check out payment with no stress.Your food is on its way!
                             </p>
+                            <img src={Table} alt="tableimage" className="image__table"/>
                 </div>
             </div>
             <div className="image__conitainer">
